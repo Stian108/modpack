d7e4c9fd29225ea222402f190f3502ab4b85e3ca
Stian <stian108@gmail.com>
2024-09-02 23:57:33 +0200

    Note to self.

--- kubejs/server_scripts/exposure.js
+++ kubejs/server_scripts/exposure.js
@@ -6,6 +6,7 @@ ExposureEvents.modifyFrameData(event => {
 ItemEvents.entityInteracted(event => {
     if (event.item.id == "minecraft:ender_pearl"
         && event.target.entityType == "entity.exposure.photograph_frame") {
+        // Add some safety checks for missing tags
         let tag = event.target.nbt.Item.tag
         event.player.teleportTo(tag.Dimension, tag.Pos[0], tag.Pos[1], tag.Pos[2], tag.Yaw, tag.Pitch)
         event.item.shrink(1)
