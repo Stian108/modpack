a2e92daf53a84200a4d1acb67a5ad3b804a6f904
stian <stian108@gmail.com>
2024-09-08 22:28:24 +0200

    Bugfixing.

--- kubejs/server_scripts/items.js
+++
@@ -1,4 +0,0 @@
-ServerEvents.tags('item', event => {
-    event.add('trinkets:head/hat', 'overweight_farming:straw_hat')
-    // TODO make unequipable in helmet slot
-})
--- kubejs/startup_scripts/items.js
+++
@@ -1,6 +0,0 @@
-ItemEvents.modification(event => {
-    event.modify('overweight_farming:straw_hat', item => {
-        item.setArmorProtection(0)
-    })
-    // TODO disable durability and enchantments
-})
